0d9d3c40b0640a1a3aee40264c8975280137470d
Julkarnine Rokey <julkarninerokey@gmail.com>
2024-07-07 18:07:07 +0600

    push

---
+++ admin/app.js
@@ -0,0 +1,26 @@
+///Edit Under This only
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB6pf0i4Y5ivhaLL2CdB7NR7ltIBRcya1s",
+  authDomain: "andrat-cede2.firebaseapp.com",
+  databaseURL: "https://andrat-cede2-default-rtdb.firebaseio.com",
+  projectId: "andrat-cede2",
+  storageBucket: "andrat-cede2.appspot.com",
+  messagingSenderId: "471271715062",
+  appId: "1:471271715062:web:2731eb45346ac684877cd7",
+};
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB6pf0i4Y5ivhaLL2CdB7NR7ltIBRcya1s",
+//   authDomain: "andrat-cede2.firebaseapp.com",
+//   databaseURL: "https://andrat-cede2-default-rtdb.firebaseio.com",
+//   projectId: "andrat-cede2",
+//   storageBucket: "andrat-cede2.appspot.com",
+//   messagingSenderId: "471271715062",
+//   appId: "1:471271715062:web:2731eb45346ac684877cd7",
+//   measurementId: "G-D37NQNPT95",
+// };
+
+////Do Not Edit Below This
+
+firebase.initializeApp(firebaseConfig);
